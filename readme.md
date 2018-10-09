c24c8cbf18d5dac92fe976b9c9edaf2442cc3176
Abhishek Gangwar <you@example.com>
2018-10-09 16:16:20 +0530

    chore(): added basic setup of stencil

---
+++ readme.md
@@ -0,0 +1,1 @@
+A stencil starter project for Creating all the web component using stencil
---
+++ stencils.config.ts
@@ -0,0 +1,11 @@
+import { Config } from '@stencil/core';
+
+// Stencil configuration is described here
+
+export const config: Config = {
+    namespace: 'stencil-web-component',
+    outputTargets: [
+        { type: 'dist' },
+        { type: 'www' }
+    ],
+}
